1f6e2e6a1652802ad5e6e31b5caac4cf5ed22a23
Samart Phrommajaree <martzing25@gmail.com>
2023-05-14 02:49:29 +0700

    src: remove not use code

--- src/restaurant/auth/auth.controller.ts
+++ src/restaurant/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 import { LoginResponse } from './type';
